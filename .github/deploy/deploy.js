5419cb40fe7a9dc450f412ab9333cb17bf305ad6
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2023-07-23 01:43:53 +0200

    Update deploy.js

--- .github/deploy/deploy.js
+++ .github/deploy/deploy.js
@@ -1,4 +1,4 @@
 import { Octokit, App } from "https://esm.sh/octokit?dts";
 const text = await Deno.readTextFile("commander.lua");
 
-console.log(">", Deno.args)
+console.log(">", Deno.env.get("GIST_DEPLOY_KEY"))
