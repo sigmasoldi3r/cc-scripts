14e538021c5c45a8ce9f6f5f59d655970c0fcf76
Pablo Blanco Celdrán <pablobc.1995@gmail.com>
2023-07-23 03:40:03 +0200

    Update deploy.js

--- .github/deploy/deploy.js
+++ .github/deploy/deploy.js
@@ -1,17 +1,26 @@
 import { Octokit, App } from "https://esm.sh/octokit?dts";
-const text = await Deno.readTextFile("commander.lua");
+
 
 const octokit = new Octokit({
   auth: Deno.env.get("GIST_DEPLOY_KEY")
 })
 
+async function cFile(where) {
+  return { [where]: { content: await Deno.readTextFile(where) } }
+}
+
 await octokit.request('PATCH /gists/{gist_id}', {
   gist_id: '5a71c8a4343a0736236b49c81a80d495',
-  description: 'Latest deploy of the Commander library ',
+  description: 'Latest deploy of the CC libraries',
   files: {
-    'commander.lua': {
-      content: text
-    }
+    ...cFile('commander.lua'),
+    ...cFile('enquirer.lua'),
+    ...cFile('monody.lua'),
+    ...cFile('grabby.lua'),
+    ...cFile('lyre.lua'),
+    ...cFile('lark.lua'),
+    ...cFile('netpipe.lua'),
+    ...cFile('robot.lua'),
   },
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
